a220979098393ee391d017c548e48ffc6387b37e
Azhagiya <azhagiyabaskar@gmail.com>
2024-04-26 22:17:21 +0530

    Terraform main file is added

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "ishu.terraform"
+}
+
+resource "aws_instance" "emicalculator_server_2" {
+  ami           = "ami-090489735582f1561"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "emicalculator_2"
+  }
+}
