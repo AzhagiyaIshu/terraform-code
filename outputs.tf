39c8acd288c713870a8cdbb837f8cb73a539c9dd
AzhagiyaIshu <162992444+AzhagiyaIshu@users.noreply.github.com>
2024-05-02 22:12:37 +0530

    output ami added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "instance_type" {
     description = "Output of the terraform code"
     value = aws_instance.myinstance.instance_type
 }
+ output "instance_ami" {
+    description = "AMI id of the instance"
+    value = aws_instance.myinstance.instance_ami
+ }
